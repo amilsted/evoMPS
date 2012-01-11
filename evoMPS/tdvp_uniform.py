1ecbacb0b6159fdd9175903da6cbc69f707df50c
Ashley Milsted <ashmilsted@gmail.com>
2012-01-11 15:52:02 +0100

    Remove beginnings of the uniform implementation from the master branch. Developement now occuring in the dev_uniform branch. the master branch.

--- evoMPS/tdvp_uniform.py
+++ evoMPS/tdvp_uniform.py
@@ -4,21 +4,14 @@ Created on Thu Oct 13 17:29:27 2011
 
 @author: Ashley Milsted
 
+WIP
+
 """
 import scipy as sp
 import scipy.linalg as la
 from scipy import *
 import nullspace as ns
 from matmul import *
-
-
-def myAop(x):
-    opres = zeros_like(A[0])
-    for i in xrange(A.shape[0]):
-        opres += matmul(None, A[i], x, H(A[i]))
-        
-    return x - opres  + r * np.trace(dot(l, x))
-
         
 class evoMPS_TDVP_Uniform:
     odr = 'C'
@@ -41,33 +34,3 @@ class evoMPS_TDVP_Uniform:
         
         for s in xrange(q):
             self.A[s] = eye(D)
-    
-    def Calc_C(self):
-        self.C.fill(0)
-        
-        AA = empty_like(self.A[0])
-        
-        for (u, v) in ndindex(self.q, self.q):
-            matmul(AA, self.A[u], self.A[v])
-            for (s, t) in ndindex(self.q, self.q):
-                C[s, t] += h_nn(n, s, t, u, v) * AA
-    
-    def Calc_K(self):
-        Hr = empty_like(self.A[0])
-        
-        AAst = empty_like(self.A[0])
-        
-        for (s, t) in ndindex(self.q, self.q):
-            matmul(AAst, self.A[s], self.A[t])
-            for (u, v) in ndindex(self.q, self.q):
-                Hr += h_nn(n, u, v, s, t) * matmul(None, AAst, self.r, H(self.A[v]), H(self.A[u]))
-                
-        QHr = Hr - self.r * sp.trace(matmul(None, self.l, Hr))
-        
-        EK = zeros_like(self.K)
-        for (s, t) in ndindex(self.q, self.q):
-            EK += matmul(None, self.A[s], self.K, H(self.A[t]))
-            
-        LHS = self.K - EK + self.r * np.trace(matmul(None, self.l, self.K))
-        
-        #Solve LHS = QHr
