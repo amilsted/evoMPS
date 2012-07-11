ccd82525eb94ddefd300152e0b495302d21caf1b
Ashley Milsted <ashmilsted@gmail.com>
2012-07-11 12:33:33 +0200

    Add allclose.pyx, an optimized "allclose" for complex128 matrices.

---
+++ evoMPS/allclose.pyx
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 21 13:21:20 2012
+
+@author: ash
+"""
+
+import cython as cy
+import numpy as np
+cimport numpy as np
+
+ctypedef np.complex128_t DTYPE_t
+
+from libc.math cimport sqrt
+
+cdef double abs_cmp(complex X):
+    return sqrt(X.real**2 + X.imag**2)
+
+@cy.boundscheck(False)
+@cy.wraparound(False)
+cpdef allclose_mat(np.ndarray[DTYPE_t, ndim=2, mode="c"] A,
+                   np.ndarray[DTYPE_t, ndim=2, mode="c"] B, 
+                   double rtol, double atol):
+    
+    assert A.shape[0] == B.shape[0] and A.shape[1] == B.shape[1]
+    
+    cdef int D1 = A.shape[0]
+    cdef int D2 = A.shape[1]    
+    
+    for i in range(D1):
+        for j in range(D2):
+            if abs_cmp(A[i, j] - B[i, j]) > atol + rtol * abs_cmp(B[i, j]):
+                return False
+    
+    return True
+    
