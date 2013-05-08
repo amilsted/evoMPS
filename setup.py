40e56c9ba0966ad863c746d96aed6d8008c61d61
Ashley Milsted <ashmilsted@gmail.com>
2013-05-08 10:42:20 +0200

    Make building extensions optional.

--- setup.py
+++ setup.py
@@ -2,12 +2,30 @@ from distutils.core import setup, Extension
 from evoMPS.version import __version__
 import numpy as np
 
+from distutils.command.build import build as _build
+
 ext_modules = [Extension("evoMPS.matmul", ["evoMPS/matmul.c"]),
                Extension("evoMPS.tdvp_common", ["evoMPS/tdvp_common.c"]),
                Extension("evoMPS.allclose", ["evoMPS/allclose.c"]),
                Extension("evoMPS.tdvp_calc_C", ["evoMPS/tdvp_calc_C.c"])]
 
-setup(name='evoMPS',
+class build(_build):         
+    def initialize_options(self):
+        _build.initialize_options(self)
+        self.disable_build_ext = 0
+        
+    def finalize_options(self):
+        _build.finalize_options(self)
+        
+        if self.disable_build_ext != 0:
+            self.sub_commands.remove(self.sub_commands[2])
+
+build.user_options += [
+            ('disable-build-ext', None,
+             "disable build-ext and use pure python mode")]
+
+setup(cmdclass={'build': build},
+      name='evoMPS',
       version=__version__,
       description='Python scripts for simulating time evolution of matrix product states',
       author='Ashley Milsted',
