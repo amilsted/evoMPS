ad1a37bc76ec66602fa93d2d6e96b7e89aad2c5c
Ashley Milsted <ashmilsted@gmail.com>
2013-05-20 10:37:36 +0200

    Trying out doing calc_x for multiple sites in parallel with cuBLAS.

--- evoMPS/tdvp_common_cuda.py
+++ evoMPS/tdvp_common_cuda.py
@@ -12,6 +12,7 @@ import pycuda.gpuarray as garr
 import pycuda.cumath as gma
 import pycuda.driver as cd
 import scikits.cuda.cublas as cb
+import scikits.cuda.linalg as cla
 
 from time import time as now #use real time, since cuda doesn't happen on the CPU!
 
@@ -144,6 +145,17 @@ def eps_r(A, x, out, tmp, tmp2, handle):
         out += tmp2
         
     return out
+    
+def eps_r_2(x, A1, A2, out, handle):
+    out.fill(0)    
+    #tmp = garr.empty((A1[0].shape[0], x.shape[1]), dtype=A1[0].dtype)
+    #tmp2 = garr.empty((tmp.shape[0], A2[0].shape[0]), dtype=A1[0].dtype)
+    for s in xrange(len(A1)):
+        tmp = cla.dot(A1[s], x, handle=handle)
+        tmp2 = cla.dot(tmp, A2[s], transb='C', handle=handle)
+        out += tmp2
+        
+    return out
 
 def get_streams(A):
     streams = []
@@ -179,6 +191,17 @@ def eps_l(A, x, out, tmp, tmp2, handle):
     for s in xrange(len(A)):
         dot_z(A[s], x, out=tmp, Ha=True, cbHandle=handle)
         dot_z(tmp, A[s], out=tmp2, cbHandle=handle)
+        out += tmp2
+        
+    return out
+    
+def eps_l_2(x, A1, A2, out, handle):
+    out.fill(0)    
+    #tmp = garr.empty((A1[0].shape[1], x.shape[1]), dtype=x.dtype)
+    #tmp2 = garr.empty((tmp.shape[0], A2[0].shape[1]), dtype=x.dtype)
+    for s in xrange(len(A1)):
+        tmp = cla.dot(A1[s], x, transa='C', handle=handle)
+        tmp2 = cla.dot(tmp, A2[s], handle=handle)
         out += tmp2
         
     return out
@@ -311,6 +334,255 @@ def test_calc_lr(D, d):
     
     x_ = eps_r_cpu(A, res)
     print la.norm(x_ - res)
+    
+def calc_x(Kp1, C, Cm1, rp1, lm2, Am1, A, Ap1, lm1_s, lm1_si, r_s, r_si, Vsh):
+    D = A.shape[2]
+    Dm1 = A.shape[1]
+    q = A.shape[0]
+    
+    handle = cb.cublasCreate()
+    
+    if not rp1 is None:
+        Grp1 = garr.to_gpu(sp.asarray(rp1))
+    if not lm2 is None:
+        Glm2 = garr.to_gpu(sp.asarray(lm2))
+    
+    Glm1_s = garr.to_gpu(sp.asarray(lm1_s))
+    Glm1_si = garr.to_gpu(sp.asarray(lm1_si))
+    
+    Gr_s = garr.to_gpu(sp.asarray(r_s))
+    Gr_si = garr.to_gpu(sp.asarray(r_si))
+    
+    GA = []
+    GAm1 = []
+    GAp1 = []
+    for s in xrange(A.shape[0]):
+        GA.append(garr.to_gpu(A[s]))
+    if not Am1 is None:
+        for s in xrange(Am1.shape[0]):
+            GAm1.append(garr.to_gpu(Am1[s]))
+    if not Ap1 is None:
+        for s in xrange(Ap1.shape[0]):
+            GAp1.append(garr.to_gpu(Ap1[s]))
+    
+    GVsh = []
+    for s in xrange(Vsh.shape[0]):
+        GVsh.append(garr.to_gpu(Vsh[s]))
+    
+    if not Cm1 is None:
+        GCm1 = []
+        for s in xrange(Cm1.shape[0]):
+            GCm1s = []
+            for t in xrange(Cm1.shape[1]):
+                GCm1s.append(garr.to_gpu(Cm1[t, s]))
+            GCm1.append(GCm1s)
+    
+    x = garr.zeros((Dm1, q * D - Dm1), dtype=A.dtype)
+    x_part = garr.empty_like(x)
+    x_subpart = garr.empty_like(GA[0])
+    
+    if not (C is None and Kp1 is None):
+        GC = []
+        for s in xrange(C.shape[0]):
+            GCs = []
+            for t in xrange(C.shape[1]):
+                GCs.append(garr.to_gpu(C[s, t]))
+            GC.append(GCs)
+            
+        GKp1 = garr.to_gpu(Kp1)
+            
+        assert (not C is None) and (not Kp1 is None)
+        x_part.fill(0)
+        for s in xrange(q):
+            x_subpart = eps_r_2(Grp1, GC[s], GAp1, x_subpart, handle) #~1st line
+            
+            x_subpart += cla.dot(GA[s], GKp1, handle=handle) #~3rd line
+    
+            x_part += cla.dot(cla.dot(x_subpart, Gr_si, handle=handle), GVsh[s], handle=handle)
+
+        x += cla.dot(Glm1_s, x_part, handle=handle)
+
+    if not lm2 is None:
+        x_part.fill(0)
+        for s in xrange(q):     #~2nd line
+            x_subpart = eps_l_2(Glm2, GAm1, GCm1[s], x_subpart, handle)
+            x_part += cla.dot(x_subpart, cla.dot(Gr_s, GVsh[s], handle=handle), handle=handle)
+        x += cla.dot(Glm1_si, x_part, handle=handle)
+        
+    cb.cublasDestroy(handle)
+    
+    return x.get()
+    
+def calc_x_G(Kp1, C, Cm1, rp1, lm2, Am1, A, Ap1, lm1_s, lm1_si, r_s, r_si, Vsh, handle=None):
+    D = A[0].shape[1]
+    Dm1 = A[0].shape[0]
+    q = len(A)
+    
+    x = garr.zeros((Dm1, q * D - Dm1), dtype=A[0].dtype)
+    x_part = garr.empty_like(x)
+    x_subpart = garr.empty_like(A[0])
+    
+    if not (C is None and Kp1 is None):
+        assert (not C is None) and (not Kp1 is None)
+        x_part.fill(0)
+        for s in xrange(q):
+            x_subpart = eps_r_2(rp1, C[s], Ap1, x_subpart, handle) #~1st line
+            
+            x_subpart += cla.dot(A[s], Kp1, handle=handle) #~3rd line
+    
+            x_part += cla.dot(cla.dot(x_subpart, r_si, handle=handle), Vsh[s], handle=handle)
+
+        x += cla.dot(lm1_s, x_part, handle=handle)
+
+    if not lm2 is None:
+        x_part.fill(0)
+        for s in xrange(q):     #~2nd line
+            x_subpart = eps_l_2(lm2, Am1, Cm1[s], x_subpart, handle)
+            x_part += cla.dot(x_subpart, cla.dot(r_s, Vsh[s], handle=handle), handle=handle)
+        x += cla.dot(lm1_si, x_part, handle=handle)
+        
+    return x
+    
+def calc_Bs(N, A, l, l_s, l_si, r, r_s, r_si, C, K, Vsh):
+    GA = []
+    for An in A:
+        if An is None:
+            GA.append(None)
+        else:
+            GAn = []
+            for Ans in An:
+                GAn.append(garr.to_gpu(Ans))
+            GA.append(GAn)
+    GA.append(None)
+    
+    Gl = []
+    Gl_s = []
+    Gl_si = []
+    for n in xrange(len(l)):
+        if l[n] is None:
+            Gl.append(None)
+            Gl_s.append(None)
+            Gl_si.append(None)
+        else:
+            Gl.append(garr.to_gpu(sp.asarray(l[n]))) #TODO: Support special types...
+            Gl_s.append(garr.to_gpu(sp.asarray(l_s[n])))
+            Gl_si.append(garr.to_gpu(sp.asarray(l_si[n])))
+    Gl.append(None)
+    Gl_s.append(None)
+    Gl_si.append(None)
+        
+    Gr = []
+    Gr_s = []
+    Gr_si = []
+    for n in xrange(len(r)):
+        if r[n] is None:
+            Gr.append(None)
+            Gr_s.append(None)
+            Gr_si.append(None)
+        else:
+            Gr.append(garr.to_gpu(sp.asarray(r[n]))) #TODO: Support special types...
+            Gr_s.append(garr.to_gpu(sp.asarray(r_s[n])))
+            Gr_si.append(garr.to_gpu(sp.asarray(r_si[n])))
+    Gr.append(None)
+    Gr_s.append(None)
+    Gr_si.append(None)
+
+    GK = []
+    for n in xrange(len(K)):
+        if K[n] is None:
+            GK.append(None)
+        else:
+            GK.append(garr.to_gpu(sp.asarray(K[n])))
+    GK.append(None)
+            
+    GVsh = []
+    for n in xrange(len(Vsh)):
+        if Vsh[n] is None:
+            GVsh.append(None)
+        else:
+            GVshn = []
+            for s in xrange(Vsh[n].shape[0]):
+                GVshn.append(garr.to_gpu(Vsh[n][s]))
+            GVsh.append(GVshn)
+    
+    GC = []
+    for n in xrange(len(C)):
+        if C[n] is None:
+            GC.append(None)
+        else:
+            GCn = []
+            for s in xrange(C[n].shape[0]):
+                GCns = []
+                for t in xrange(C[n].shape[1]):
+                    GCns.append(garr.to_gpu(C[n][s, t]))
+                GCn.append(GCns)
+            GC.append(GCn)
+    GC.append(None)
+    
+    GCts = []
+    for n in xrange(len(GC)):
+        if GC[n] is None:
+            GCts.append(None)
+        else:
+            GCtsn = []
+            for t in xrange(len(GC[n])):
+                GCtsns = []
+                for s in xrange(len(GC[n][0])):
+                    GCtsns.append(GC[n][s][t])
+                GCtsn.append(GCtsns)
+            GCts.append(GCtsn)
+            
+    hdl = cb.cublasCreate()
+    
+    num_strms = 10
+    
+    curr_stream = cb.cublasGetStream(hdl)
+    
+    sites_per_strm = max((N) / num_strms, 1)
+    #print "sites_per_stream = ", sites_per_strm
+    
+    strms = []
+    for i in xrange(N / sites_per_strm):
+        strms.append(cd.Stream())
+    
+    GB = [None]
+    for n in xrange(1, N + 1):
+        if (n - 1) % sites_per_strm == 0:
+            #print n
+            #print "strm = ", (n - 1) / sites_per_strm
+            cb.cublasSetStream(hdl, strms[(n - 1) / sites_per_strm].handle)
+        if not Vsh[n] is None:
+            if n > 1:
+                Glm2 = Gl[n - 2]
+            else:
+                Glm2 = None
+                
+            Gx = calc_x_G(GK[n + 1], GC[n], GCts[n - 1], Gr[n + 1], Glm2, GA[n - 1], GA[n],
+                          GA[n + 1], Gl_s[n - 1], Gl_si[n - 1], Gr_s[n], Gr_si[n], GVsh[n], handle=hdl)
+            GBn = []
+            for s in xrange(A[n].shape[0]):
+                GBns = cla.dot(Gl_si[n - 1], Gx, handle=hdl) 
+                GBns = cla.dot(GBns, GVsh[n][s], transb='C', handle=hdl)
+                GBns = cla.dot(GBns, Gr_si[n], handle=hdl)
+                GBn.append(GBns)
+            GB.append(GBn)
+        else:
+            GB.append(None)
+            
+    cb.cublasSetStream(hdl, curr_stream)    
+    cb.cublasDestroy(hdl)
+    
+    B = [None]
+    for n in xrange(1, N + 1):
+        if GB[n] is None:
+            B.append(None)
+        else:
+            Bn = sp.empty_like(A[n])
+            for s in xrange(A[n].shape[0]):
+                Bn[s] = GB[n][s].get()
+            B.append(Bn)
+        
+    return B
     
 if __name__ == '__main__':
     test_calc_lr(512, 16)
