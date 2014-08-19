46960a61ee99febdd83d5490824ccda2796325ae
Ashley Milsted <ashmilsted@gmail.com>
2014-08-19 18:35:53 +0200

    Update example for testing dynamics.py.

--- examples/heisenberg_af_uniform.py
+++ examples/heisenberg_af_uniform.py
@@ -16,20 +16,21 @@ import evoMPS.dynamics as dy
 """
 First, we set up some global variables to be used as parameters.
 """
+seed = 8
 
 S = 0.5                       #Spin: Can be 0.5 or 1.
-block_length = 2              #Translation-invariant block length
-bond_dim = 8                 #The maximum bond dimension
+block_length = 1              #Translation-invariant block length
+bond_dim = 32                 #The maximum bond dimension
 
 Jx = 1.00                     #Interaction factors (Jx == Jy == Jz > 0 is the antiferromagnetic Heisenberg model)
 Jy = 1.00
-Jz = 1.00
+Jz = 0.00
 
-tol = 1E-10                #Ground state tolerance (norm of projected evolution vector)
+tol = 1E-6                #Ground state tolerance (norm of projected evolution vector)
 
-step = 0.05                    #Imaginary time step size
+step = 0.07                    #Imaginary time step size
 
-load_saved_ground = True      #Whether to load a saved ground state (if it exists)
+load_saved_ground = False      #Whether to load a saved ground state (if it exists)
 
 auto_truncate = False         #Whether to reduce the bond-dimension if any Schmidt coefficients fall below a tolerance.
 zero_tol = 1E-20              #Zero-tolerance for the Schmidt coefficients squared (right canonical form)
@@ -94,6 +95,8 @@ def get_ham(Jx, Jy, Jz):
 """
 Now we are ready to create an instance of the evoMPS class.
 """
+sp.random.seed(seed)
+
 s = tdvp.EvoMPS_TDVP_Uniform(bond_dim, qn, get_ham(Jx, Jy, Jz), L=block_length)
 s.zero_tol = zero_tol
 s.sanity_checks = sanity_checks
@@ -141,7 +144,7 @@ if __name__ == '__main__':
     print "\t".join(col_heads)
     print
 
-    def cbf(i, s):
+    def cbf(s, i, **kwargs):
         H.append(s.h_expect.real)
 
         row = [str(i)]
@@ -163,13 +166,86 @@ if __name__ == '__main__':
             exSzs.append("%.3g" % s.expect_1s(Sz, k=k).real)
         row += exSzs
         
-        row.append("%.6g" % s.eta.real.sum())
+        row.append("%.6g" % s.eta)
+
+        row.append(str(kwargs))
 
         print "\t".join(row)
 
 
     if not loaded:
-        s, steps = dy.find_ground(s, tol=tol, dtau=step, cb_func=cbf)
+        #s, steps, tau = dy.opt_im_time(s, tol=tol, dtau0=step, cb_func=cbf, max_itr=10)
+        #dy.opt_conj_grad2(s)
+        #exit()
+        
+        import time
+        import sys
+        Ts = []
+        hs = []
+        Ns = []
+        Szs = []
+        etas = []
+
+        test_type = 2
+
+        if test_type == 1:
+            t0 = time.clock()
+            s, steps = dy.find_ground(s, tol=tol, dtau=step, cb_func=cbf)
+            Ts.append(time.clock() - t0)
+            hs.append(s.h_expect.real)
+            Ns.append("find ground")
+            Szs.append(s.expect_1s(Sz, k=0).real)
+
+            sp.random.seed(seed)
+            s.randomize()
+            t0 = time.clock()
+            s, steps = dy.find_ground(s, tol=tol, dtau=step, cb_func=cbf, gap_gd=True)
+            Ts.append(time.clock() - t0)
+            hs.append(s.h_expect.real)
+            Ns.append("find ground, GD")
+            Szs.append(s.expect_1s(Sz, k=0).real)
+
+            sp.random.seed(seed)
+            s.randomize()
+            t0 = time.clock()
+            s, steps, tau = dy.opt_im_time(s, tol=tol, dtau0=step, cb_func=cbf)
+            Ts.append(time.clock() - t0)
+            hs.append(s.h_expect.real)
+            Ns.append("im time")
+            Szs.append(s.expect_1s(Sz, k=0).real)
+
+            sp.random.seed(seed)
+            s.randomize()
+            t0 = time.clock()
+            s, steps = dy.opt_conj_grad(s, tol=tol, h0=step, cb_func=cbf, reset_every=5)
+            Ts.append(time.clock() - t0)
+            hs.append(s.h_expect.real)
+            Ns.append("CG")
+            Szs.append(s.expect_1s(Sz, k=0).real)
+
+            map(lambda x: sys.stdout.write("\t".join(map(str, x)) + "\n"), zip(Ts, hs, Szs, Ns))
+        else:
+            for cg_steps in xrange(1, 11):
+                sp.random.seed(seed)
+                s.randomize()
+                t0 = time.clock()
+                #try:
+                s, steps = dy.find_ground(s, tol=tol, dtau=step, cb_func=cbf, gap_gd=False, CG_max=cg_steps, max_itr=100000)
+                Ts.append(time.clock() - t0)
+                hs.append(s.h_expect.real)
+                Ns.append(cg_steps)
+                Szs.append(s.expect_1s(Sz, k=0).real)
+                etas.append(s.eta.sum().real)
+                #except:
+                #    pass
+
+            hs = sp.array(hs)
+            hs = hs - hs.min()
+            hs = map(lambda x: "%.3e" % x, hs)
+            Szs = map(lambda x: "%.3e" % x, Szs)
+            map(lambda x: sys.stdout.write("\t".join(map(str, x)) + "\n"), zip(Ts, etas, hs, Szs, Ns))
+
+        exit()
         s.save_state(grnd_fname)
 
     """
