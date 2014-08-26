0766ec508c0bf16aede9c0aaa3ed4c3374c43715
Ashley Milsted <ashmilsted@gmail.com>
2014-08-26 17:21:19 +0200

    Fix cg to restore CF when resetting. Add gap to opt_grad_desc, since it benefits it greatly. Plain CG does not need it now. Rename some stuff.

--- evoMPS/dynamics.py
+++ evoMPS/dynamics.py
@@ -33,7 +33,7 @@ def evolve(sys, t, dt=0.01, integ="euler", dynexp=True, maxD=None, cb_func=None)
         
     return sys    
 
-def find_ground(sys, tol=1E-6, dtau=0.04, use_CG=True, CG_gap=5, CG_max=5, 
+def find_ground(sys, tol=1E-6, h_init=0.04, use_CG=True, CG_gap=5, CG_max=5, 
                 CG_tol=1000,
                 max_itr=10000, gap_gd=False, cb_func=None):
     j = 0
@@ -43,7 +43,7 @@ def find_ground(sys, tol=1E-6, dtau=0.04, use_CG=True, CG_gap=5, CG_max=5,
     else:
         cb_wrap = None
         
-    dtau_base = dtau
+    dtau = h_init
     h0 = None
     while j < max_itr:
         if gap_gd:
@@ -51,7 +51,7 @@ def find_ground(sys, tol=1E-6, dtau=0.04, use_CG=True, CG_gap=5, CG_max=5,
                                        max_itr=min(CG_gap, max_itr - j), 
                                        cb_func=cb_wrap)
         else:
-            sys, dj, tau, dtau = opt_im_time(sys, tol=tol, dtau_base=dtau_base, 
+            sys, dj, tau, dtau = opt_im_time(sys, tol=tol, dtau_base=h_init, 
                                              dtau0=dtau, 
                                              max_itr=min(CG_gap, max_itr - j), 
                                              cb_func=cb_wrap, auto_trunc=True)
@@ -64,7 +64,7 @@ def find_ground(sys, tol=1E-6, dtau=0.04, use_CG=True, CG_gap=5, CG_max=5,
                 CG_max_itr = min(CG_max, max_itr - j)
             else:
                 CG_max_itr = min(1, max_itr - j)
-            sys, dj, h0 = opt_conj_grad(sys, tol=tol, h_init=dtau, h0_prev=h0, 
+            sys, dj, h0 = opt_conj_grad(sys, tol=tol, h_init=h_init, h0_prev=h0, 
                                         max_itr=CG_max_itr, 
                                         reset_every=CG_max, cb_func=cb_wrap)
             j += dj
@@ -135,7 +135,8 @@ def opt_im_time(sys, tol=1E-6, dtau_base=0.04, dtau0=0.04, max_itr=10000, cb_fun
         
     return sys, i + 1, tau, dtau
 
-def opt_conj_grad(sys, tol=1E-6, h_init=0.01, h0_prev=None, reset_every=10, max_itr=10000, cb_func=None):
+def opt_conj_grad(sys, tol=1E-6, h_init=0.01, h0_prev=None, reset_every=20, 
+                  max_itr=10000, cb_func=None):
     B = None
     B_grad = None
     if not h0_prev is None:
@@ -148,11 +149,13 @@ def opt_conj_grad(sys, tol=1E-6, h_init=0.01, h0_prev=None, reset_every=10, max_
     #e = 0
     i = -1
     for i in xrange(max_itr):
-        sys.update(restore_CF=i == 0)
+        reset = i % reset_every == 0
+        sys.update(restore_CF=reset) #This simple CG works much better without restore_CF within a run.
+                                     #With restore_CF, one would have to transform B_prev to match.
         
-        B, B_grad, BgdotBg, h, g0 = sys.calc_B_CG(B, BgdotBg, h_init, dtau_prev=h, g0_prev=g0,
-                                          reset=i % reset_every == 0, 
-                                          B_prev=B_grad, use_PR=True)
+        B, B_grad, BgdotBg, h, g0 = sys.calc_B_CG(B, BgdotBg, h_init, dtau_prev=h, 
+                                                  g0_prev=g0, reset=reset, 
+                                                  B_prev=B_grad, use_PR=True)
 
         if not cb_func is None:
             cb_func(sys, i, h=h)
@@ -164,12 +167,36 @@ def opt_conj_grad(sys, tol=1E-6, h_init=0.01, h0_prev=None, reset_every=10, max_
 
         if i == 0:
             h0 = h #Store the first (steepest descent) step
-            h = h_init #second step is usually far shorter than the first!        
+            h = h_init #second step is usually far shorter than the first (GD) step!
 
     return sys, i + 1, h0
 
-def opt_grad_descent(sys, tol=1E-6, h0=0.01, max_itr=10000, cb_func=None):
-    return opt_conj_grad(sys, tol=tol, h0=h0, max_itr=max_itr, reset_every=1, cb_func=cb_func)
+def opt_grad_descent(sys, tol=1E-6, h_init=0.01, max_itr=10000, im_gap=5, 
+                     gap_every=1, cb_func=None):
+    j = 0
+    if not cb_func is None:
+        def cb_wrap(sys, i, **kwargs):
+            cb_func(sys, j + i, **kwargs)
+    else:
+        cb_wrap = None
+        
+    dtau = h_init
+    h0 = None
+    while j < max_itr:
+        sys, dj, tau, dtau = opt_im_time(sys, tol=tol, dtau_base=h_init, 
+                                         dtau0=dtau, 
+                                         max_itr=min(im_gap, max_itr - j), 
+                                         cb_func=cb_wrap, auto_trunc=True)
+        j += dj
+        
+        sys, dj, h0 = opt_conj_grad(sys, tol=tol, h_init=h_init, h0_prev=h0, 
+                                    max_itr=min(gap_every, max_itr - j), 
+                                    reset_every=1, cb_func=cb_wrap)
+        j += dj
+            
+        if sys.eta < tol: #Note: In case tol not reached, this value is out of date, but that's okay.
+            break  
+    return sys, j
 
 
 def _opt_conj_grad2_f(x, sys, pars):
