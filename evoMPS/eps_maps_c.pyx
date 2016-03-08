a0636d6712a0732e410673b5bb4f929c01fb8ff9
U-PI\amilsted <amilsted@PI0007446.pi.local>
2016-03-08 11:39:48 -0500

    Use openmp to thread the eps maps across the physical dimension d. Requires compilation using openmp support to work.

--- evoMPS/eps_maps_c.pyx
+++ evoMPS/eps_maps_c.pyx
@@ -9,6 +9,8 @@ import matmul as mm
 import cython as cy
 import numpy as np
 cimport numpy as np
+from cython.parallel cimport parallel, prange, threadid
+cimport openmp
 
 from libc.stdlib cimport malloc, free
 
@@ -35,6 +37,8 @@ cdef extern from "cblas.h":
 
     void cblas_zscal(int N, double *alpha, double *X, int incX) nogil
     
+    void cblas_zaxpy(int n, double *a, double *x, int incx, double *y, int incy) nogil
+    
 #cdef extern from "capsule.h":
 #    void* SMCapsule_AsVoidPtr(object ptr)
 #
@@ -47,6 +51,9 @@ cdef extern from "cblas.h":
 #cdef zdotu_t *zdotu = <zdotu_t*>SMCapsule_AsVoidPtr(scipy.linalg.blas.get_blas_funcs('dotu', dtype=complex128)._cpointer)
 #cdef zgemv_t *zgemv = <zgemv_t*>SMCapsule_AsVoidPtr(scipy.linalg.blas.get_blas_funcs('gemv', dtype=complex128)._cpointer)
 
+cdef inline int int_min(int a, int b) nogil: return a if a <= b else b
+cdef inline int int_max(int a, int b) nogil: return a if a > b else b
+
 ctypedef np.complex128_t cplx
 ctypedef np.complex128_t[::1] cmp1d
 ctypedef np.complex128_t[:, ::1] cmp2d
@@ -57,67 +64,157 @@ cpdef np.ndarray eps_l_noop(x, ndcmp3d A1, ndcmp3d A2):
     cdef np.ndarray ndout = np.empty((A1.shape[2], A2.shape[2]), dtype=np.complex128)
     return eps_l_noop_inplace(x, A1, A2, ndout)
 
+@cy.boundscheck(False)
+@cy.wraparound(False)
 cpdef np.ndarray eps_l_noop_inplace(x, ndcmp3d A1, ndcmp3d A2, ndcmp2d ndout):
     ndout.fill(0)
     cdef cmp2d out = ndout
-    cdef cmp2d A1Hx
+    cdef cmp3d outs
+    cdef int Nout = out.shape[0] * out.shape[1]
+    cdef complex alpha = 1.0
     
+    cdef int d = A1.shape[0]
+    cdef int i, si, sf
+    cdef int num_chunks = int_min(openmp.omp_get_max_threads(), d)
+    cdef int lpc = int_max(1, d / num_chunks) #loops per chunk
+
     if not A1.flags['C_CONTIGUOUS']:
         A1 = np.ascontiguousarray(A1)
     if not A2.flags['C_CONTIGUOUS']:
         A2 = np.ascontiguousarray(A2)
+
+    cdef cmp3d A1_ = A1
+    cdef cmp3d A2_ = A2
+    cdef cmp2d x_
+    cdef cmp1d xd_
+    cdef cmp3d A1Hx
     
-    assert A1.shape[0] == A2.shape[0], "dimension mismatch"
-    assert A1.shape[1] == x.shape[0], "dimension mismatch"
-    assert x.shape[1] == A2.shape[1], "dimension mismatch"
+    assert A1_.shape[0] == A2_.shape[0], "dimension mismatch"
+    assert A1_.shape[1] == x.shape[0], "dimension mismatch"
+    assert x.shape[1] == A2_.shape[1], "dimension mismatch"
+    
+    if num_chunks > 1: #only allocate extra memory if we spawn extra threads
+        outs = np.zeros((num_chunks, out.shape[0], out.shape[1]), dtype=np.complex128) 
     
     if isinstance(x, mm.eyemat):
-        eps_l_noop_id(A1, A2, out)
+        if num_chunks == 1:
+            eps_l_noop_id(A1_, A2_, out)
+        else:
+            for i in prange(num_chunks, nogil=True, num_threads=num_chunks):
+                si = i * lpc
+                sf = d if i == num_chunks - 1 else (i + 1) * lpc
+                eps_l_noop_id(A1_[si:sf,:,:], A2_[si:sf,:,:], outs[i,:,:])
     elif isinstance(x, mm.simple_diag_matrix):
-        d = x.diag
-        if not d.flags['C_CONTIGUOUS']:
-            d = np.ascontiguousarray(d)
-        A1Hx = np.zeros((A1.shape[2], x.shape[1]), dtype=np.complex128)
-        eps_l_noop_diag(d, A1, A2, A1Hx, out)
+        if x.diag.flags['C_CONTIGUOUS']:
+            xd_ = x.diag
+        else:
+            xd_ = np.ascontiguousarray(x.diag)
+        A1Hx = np.zeros((num_chunks, A1_.shape[2], xd_.shape[0]), dtype=np.complex128)
+        if num_chunks == 1:
+            eps_l_noop_diag(xd_, A1_, A2_, A1Hx[0,:,:], out)
+        else:
+            for i in prange(num_chunks, nogil=True, num_threads=num_chunks):
+                si = i * lpc
+                sf = d if i == num_chunks - 1 else (i + 1) * lpc
+                eps_l_noop_diag(xd_, A1_[si:sf,:,:], A2_[si:sf,:,:], A1Hx[i,:,:], outs[i,:,:])
     else:
-        if not x.flags['C_CONTIGUOUS']:
-            x = np.ascontiguousarray(x)
-        A1Hx = np.zeros((A1.shape[2], x.shape[1]), dtype=np.complex128)
-        eps_l_noop_dense(x, A1, A2, A1Hx, out)
+        if x.flags['C_CONTIGUOUS']:
+            x_ = x
+        else:
+            x_ = np.ascontiguousarray(x)
+        A1Hx = np.zeros((num_chunks, A1_.shape[2], x_.shape[1]), dtype=np.complex128)
         
+        if num_chunks == 1:
+            eps_l_noop_dense(x_, A1_, A2_, A1Hx[0,:,:], out)
+        else:
+            outs = np.zeros((num_chunks, out.shape[0], out.shape[1]), dtype=np.complex128) 
+            for i in prange(num_chunks, nogil=True, num_threads=num_chunks):
+                si = i * lpc
+                sf = d if i == num_chunks - 1 else (i + 1) * lpc
+                eps_l_noop_dense(x_, A1_[si:sf,:,:], A2_[si:sf,:,:], A1Hx[i,:,:], outs[i,:,:])
+                    
+    if num_chunks > 1: #add thread results up in case we used threads
+        with nogil:
+            for i in xrange(0, num_chunks):
+                cblas_zaxpy(Nout, <double *>&alpha, <double *>&outs[i, 0, 0], 1, <double *>&out[0, 0], 1)
+
     return ndout
 
 cpdef np.ndarray eps_r_noop(x, ndcmp3d A1, ndcmp3d A2):
     cdef np.ndarray ndout = np.empty((A1.shape[1], A2.shape[1]), dtype=np.complex128)
     return eps_r_noop_inplace(x, A1, A2, ndout)
-        
+
+@cy.boundscheck(False)
+@cy.wraparound(False)
 cpdef np.ndarray eps_r_noop_inplace(x, ndcmp3d A1, ndcmp3d A2, ndcmp2d ndout):
     ndout.fill(0)
     cdef cmp2d out = ndout
-    cdef cmp2d A1x
+    cdef cmp3d outs
+    cdef int Nout = out.shape[0] * out.shape[1]
+    cdef complex alpha = 1.0
+    
+    cdef int d = A1.shape[0]
+    cdef int i, si, sf
+    cdef int num_chunks = int_min(openmp.omp_get_max_threads(), d)
+    cdef int lpc = int_max(1, d / num_chunks) #loops per chunk
     
     if not A1.flags['C_CONTIGUOUS']:
         A1 = np.ascontiguousarray(A1)
     if not A2.flags['C_CONTIGUOUS']:
         A2 = np.ascontiguousarray(A2)
+        
+    cdef cmp3d A1_ = A1
+    cdef cmp3d A2_ = A2
+    cdef cmp2d x_
+    cdef cmp1d xd_
+    cdef cmp3d A1x
     
     assert A1.shape[0] == A2.shape[0], "dimension mismatch"
     assert A1.shape[2] == x.shape[0], "dimension mismatch"
     assert x.shape[1] == A2.shape[2], "dimension mismatch"
     
+    if num_chunks > 1: #only allocate extra memory if we spawn extra threads
+        outs = np.zeros((num_chunks, out.shape[0], out.shape[1]), dtype=np.complex128) 
+    
     if isinstance(x, mm.eyemat):
-        eps_r_noop_id(A1, A2, out)
+        if num_chunks == 1:
+            eps_r_noop_id(A1_, A2_, out)
+        else:
+            for i in prange(num_chunks, nogil=True, num_threads=num_chunks):
+                si = i * lpc
+                sf = d if i == num_chunks - 1 else (i + 1) * lpc
+                eps_r_noop_id(A1_[si:sf,:,:], A2_[si:sf,:,:], outs[i,:,:])
     elif isinstance(x, mm.simple_diag_matrix):
-        d = x.diag
-        if not d.flags['C_CONTIGUOUS']:
-            d = np.ascontiguousarray(d)
-        A1x = np.zeros((A1.shape[1], x.shape[1]), dtype=np.complex128)
-        eps_r_noop_diag(d, A1, A2, A1x, out)
+        if x.diag.flags['C_CONTIGUOUS']:
+            xd_ = x.diag
+        else:
+            xd_ = np.ascontiguousarray(x.diag)
+        A1x = np.zeros((num_chunks, A1_.shape[1], xd_.shape[0]), dtype=np.complex128)
+        if num_chunks == 1:
+            eps_r_noop_diag(xd_, A1_, A2_, A1x[0,:,:], out)
+        else:
+            for i in prange(num_chunks, nogil=True, num_threads=num_chunks):
+                si = i * lpc
+                sf = d if i == num_chunks - 1 else (i + 1) * lpc
+                eps_r_noop_diag(xd_, A1_[si:sf,:,:], A2_[si:sf,:,:], A1x[i,:,:], outs[i,:,:])
     else:
-        if not x.flags['C_CONTIGUOUS']:
-            x = np.ascontiguousarray(x)
-        A1x = np.zeros((A1.shape[1], x.shape[1]), dtype=np.complex128)
-        eps_r_noop_dense(x, A1, A2, A1x, out)
+        if x.flags['C_CONTIGUOUS']:
+            x_ = x
+        else:
+            x_ = np.ascontiguousarray(x)
+        A1x = np.zeros((num_chunks, A1_.shape[1], x_.shape[1]), dtype=np.complex128)
+        if num_chunks == 1:
+            eps_r_noop_dense(x_, A1_, A2_, A1x[0,:,:], out)
+        else:
+            for i in prange(num_chunks, nogil=True, num_threads=num_chunks):
+                si = i * lpc
+                sf = d if i == num_chunks - 1 else (i + 1) * lpc
+                eps_r_noop_dense(x_, A1_[si:sf,:,:], A2_[si:sf,:,:], A1x[i,:,:], outs[i,:,:])
+        
+    if num_chunks > 1: #add thread results up in case we used threads
+        with nogil:
+            for i in xrange(0, num_chunks):
+                cblas_zaxpy(Nout, <double *>&alpha, <double *>&outs[i, 0, 0], 1, <double *>&out[0, 0], 1)
 
     return ndout
     
