c2973671f49e4794151b786c15a319a6a7566172
Ashley Milsted <ashmilsted@gmail.com>
2018-03-01 15:11:04 -0500

    Try to use numpy's includes to find BLAS.

--- setup_ext_pure_c.py
+++ setup_ext_pure_c.py
@@ -5,13 +5,15 @@ Created on Mon Jan 12 14:18:20 2015
 @author: ash
 """
 
+import numpy
 from distutils.core import setup
 from distutils.extension import Extension
 
 setup(
     name='evoMPS-ext-pure-c',
     ext_modules = [Extension("evoMPS.eps_maps_c", ["evoMPS/eps_maps_c.c"], 
-                             libraries=["cblas"],
+                             libraries=["blas"],
+                             include_dirs=[numpy.get_include()],
                              extra_compile_args=['-fopenmp'],
                              extra_link_args=['-fopenmp'])] #for openmp with gcc
 )
